d8dfebbe93b93744ec5adfaee0d8d540d55251b5
HarumotoHayako <harumotohayako@gmail.com>
2025-11-06 15:28:52 -0800

    Create streamers.js

---
+++ streamers.js
@@ -0,0 +1,13 @@
+export const streamers = [
+  "amariette", "Atemni", "AureliaAlarie", "BBQdPotato", "bunbibi", "Candy_Hearts",
+  "CarminVwV", "CiBeans", "DairyDeliveryForce", "doridoriix", "DorkAltDel", "Doughkey",
+  "dualmischief_vt", "Ecchi4Kitsune", "EndyEnigma", "Fasla415", "Geekxie", "GhillLoonie",
+  "Glorpeep", "GoblinFia", "HerrWesti", "JackFrostVT", "junejunejune", "JustAddBleach",
+  "jonkanpon", "Kat_actual", "KeananNoot", "kiyan_f", "Kiyokamis", "Kopadot", "Luoonar",
+  "LuxHecate", "Novis_VT", "M1iinty", "mahogany_wood_", "MayaHirawa", "meowdekaiser_",
+  "meowiiaa", "MoeTheFrog", "Mxsk___", "panda_iz_daddy", "piyalette", "ProfessorKilobyte",
+  "ProjectUniverseVT", "RemuTenumbrae", "remziie", "RobinandNoxy", "sabbythekiwi",
+  "ShenSratos", "ShuraAlka", "sleepydewpad", "spacecatnyaa", "Squiddy_Nia", "S_Z_B_",
+  "TanukiMask", "TheEternalOmenKuri", "theyClem", "TragedyLink", "TrinityNYC",
+  "ValeTheBread", "VityaNova69", "WhitEyeVT", "YureiTheYokai", "ZionDenial", "Corvus_Silanchen"
+];
